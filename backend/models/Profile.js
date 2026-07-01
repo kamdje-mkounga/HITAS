332e376732b6786d09a13d58a93a29cc28c984f4
Roberto <ronaldokamdje@gmail.com>
2026-07-01 12:06:17 +0530

    Auto Backup 01-07-2026 12:06:16.90

--- backend/models/Profile.js
+++ backend/models/Profile.js
@@ -3,16 +3,43 @@ const mongoose = require('mongoose');
 const ProfileSchema = new mongoose.Schema({
   user: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: 'User' // Doit correspondre exactement au nom de ton modèle User
+    ref: 'User'
   },
-  firstName: { type: String, required: true },
-  lastName: { type: String, required: true },
-  promotion: { type: String, required: true },
-  specialty: { type: String, required: true },
-  currentLocation: { type: String, required: true },
-  bio: { type: String },
-  skills: { type: [String] },
-  avatar: { type: String } // Enregistre le chemin de l'image (ex: /uploads/avatar-123.jpg)
+
+  firstName: {
+    type: String,
+    required: true
+  },
+
+  lastName: {
+    type: String,
+    required: true
+  },
+
+  promotion: {
+    type: String,
+    required: true
+  },
+
+  specialty: {
+    type: String,
+    required: true
+  },
+
+  currentLocation: {
+    type: String,
+    required: true
+  },
+
+  bio: String,
+
+  skills: [String],
+
+  avatar: String,
+
+  // NEW
+  avatarPath: String
+
 }, { timestamps: true });
 
 module.exports = mongoose.model('Profile', ProfileSchema);
